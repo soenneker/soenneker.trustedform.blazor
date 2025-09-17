1f3a1414fe9c0518a72de095c6b3badcdd390339
Jake Soenneker <jake@soenneker.com>
2025-09-17 12:04:35 -0500

    No more blazorise

--- test/Soenneker.TrustedForm.Blazor.Demo/Program.cs
+++ test/Soenneker.TrustedForm.Blazor.Demo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Blazorise;
-using Blazorise.Bootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -34,10 +32,6 @@ public sealed class Program
             });
 
             builder.Services.AddTrustedFormInteropAsScoped();
-
-            builder.Services
-                .AddBlazorise()
-                .AddBootstrapProviders();
 
             WebAssemblyHost host = builder.Build();
 
