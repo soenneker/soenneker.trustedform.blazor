5928caa9e5ef2749c7a98fef580678abecfd687a
Jake Soenneker <jake@soenneker.com>
2026-01-26 19:17:39 -0600

    Adds CancellationScope

--- src/TrustedFormInterop.cs
+++ src/TrustedFormInterop.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.JSInterop;
+using Microsoft.JSInterop;
 using Soenneker.Asyncs.Initializers;
 using Soenneker.Blazor.Utils.ResourceLoader.Abstract;
+using Soenneker.Extensions.CancellationTokens;
 using Soenneker.Extensions.ValueTask;
 using Soenneker.TrustedForm.Blazor.Abstract;
 using Soenneker.TrustedForm.Blazor.Options;
+using Soenneker.Utils.CancellationScopes;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,6 +17,7 @@ public sealed class TrustedFormInterop : ITrustedFormInterop
     private readonly IJSRuntime _jsRuntime;
     private readonly IResourceLoader _resourceLoader;
     private readonly AsyncInitializer _scriptInitializer;
+    private readonly CancellationScope _cancellationScope = new();
     private bool _isRecording;
 
     private const string _modulePath = "Soenneker.TrustedForm.Blazor/js/trustedforminterop.js";
@@ -34,42 +37,72 @@ public sealed class TrustedFormInterop : ITrustedFormInterop
     public async ValueTask Init(string elementId, TrustedFormConfiguration configuration, DotNetObjectReference<TrustedForm> dotNetCallback,
         CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.init", cancellationToken, elementId, configuration, dotNetCallback).NoSync();
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.init", linked, elementId, configuration, dotNetCallback).NoSync();
+        }
     }
 
     public async ValueTask CreateObserver(string elementId, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.createObserver", cancellationToken, elementId).NoSync();
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.createObserver", linked, elementId).NoSync();
+        }
     }
 
     public async ValueTask<string?> GetCertUrl(string elementId, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
 
-        return await _jsRuntime.InvokeAsync<string?>($"{_moduleName}.getCertUrl", cancellationToken, elementId);
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+
+            return await _jsRuntime.InvokeAsync<string?>($"{_moduleName}.getCertUrl", linked, elementId);
+        }
     }
 
     public async ValueTask<string?> GetCertUrlForSingleElement(CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
 
-        return await _jsRuntime.InvokeAsync<string?>($"{_moduleName}.getCertUrlForSingleElement", cancellationToken);
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+
+            return await _jsRuntime.InvokeAsync<string?>($"{_moduleName}.getCertUrlForSingleElement", linked);
+        }
     }
 
     public async ValueTask Stop(CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.stop", cancellationToken).NoSync();
-        _isRecording = false;
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.stop", linked).NoSync();
+            _isRecording = false;
+        }
     }
 
     public async ValueTask Start(CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.start", cancellationToken).NoSync();
-        _isRecording = true;
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.start", linked).NoSync();
+            _isRecording = true;
+        }
     }
 
     public async ValueTask StartIfNotRunning(CancellationToken cancellationToken = default)
@@ -77,9 +110,14 @@ public sealed class TrustedFormInterop : ITrustedFormInterop
         if (_isRecording)
             return;
 
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.start", cancellationToken).NoSync();
-        _isRecording = true;
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.start", linked).NoSync();
+            _isRecording = true;
+        }
     }
 
     public bool IsRecording()
@@ -89,13 +127,19 @@ public sealed class TrustedFormInterop : ITrustedFormInterop
 
     public async ValueTask Finalize(string elementId, TrustedFormConfiguration configuration, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken).NoSync();
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.finalize", cancellationToken, elementId, configuration).NoSync();
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked).NoSync();
+            await _jsRuntime.InvokeVoidAsync($"{_moduleName}.finalize", linked, elementId, configuration).NoSync();
+        }
     }
 
     public async ValueTask DisposeAsync()
     {
         await _resourceLoader.DisposeModule(_modulePath).NoSync();
         await _scriptInitializer.DisposeAsync().NoSync();
+        await _cancellationScope.DisposeAsync().NoSync();
     }
 }
